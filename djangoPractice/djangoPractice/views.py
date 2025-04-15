bff7cec76f3ee83bbe4a9535c7c33635827fdc44
RaviAmanYadav <amanbittuyadav@gmail.com>
2025-04-15 20:33:41 +0530

    Upto Home page render is completed

---
+++ djangoPractice/djangoPractice/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def Home(response):
+    return render(response, "index.html")
+
+
+def About(response):
+    return HttpResponse("<h1>About Page<h1>")
+
+
+def Contact(response):
+    return HttpResponse("<h1>Contact Page<h1>")
